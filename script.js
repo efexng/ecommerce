41b6e2a3d81ac181bbbbd0add8edfa0fb070f142
efexng <efeapoki2020@gmail.com>
2024-04-22 17:43:39 +0100

    60%

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-const imgs = document.querySelector
+const imgs = document.querySelectorAll('.header-slider ul img');
+const prev_btn = document.querySelector('.control_prev');
+const next_btn = document.querySelector('.control_next');
+
+
+let n = 0;
+
+function chnageSlider() {
+    for (let i = 0; i < imgs.length; i++) {
+        imgs[i].style.display = 'none';
+    }
+    imgs[n].style.display = 'block';
+}
+
+chnageSlider();
+
+prev_btn.addEventListener('click', (e)=> {
+    if (n > 0) {
+        n--;
+    } else {
+        n = imgs.length - 1;
+    }
+    chnageSlider();
+});
+
+next_btn.addEventListener('click', (e)=> {
+    if (n < imgs.length - 1) {
+        n++;
+    } else {
+        n = 0;
+    }
+    chnageSlider();
+});
+
+
+const scrollContainder = document.querySelectorAll('.products')
+for ( const item of scrollContainder) {
+    item.addEventListener('wheel', (evt)=>{
+        evt.preventDefault();
+        item.scrollLeft += evt.deltaY;
+    })
+}
